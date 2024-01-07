360f54ab905327da1fe81c7709eed9aa86f2e706
rahul <rahulsahani285@gmail.com>
2024-01-07 14:42:27 +0530

    This is my first commit

---
+++ src/test/java/rahul/tests/ErrorValidationsTest.java
@@ -0,0 +1,51 @@
+package rahul.tests;
+
+import org.testng.annotations.Test;
+
+import rahul.TestComponents.BaseTest;
+import rahul.TestComponents.Retry;
+import rahul.pageobjects.CartPage;
+import rahul.pageobjects.CheckoutPage;
+import rahul.pageobjects.ConfirmationPage;
+import rahul.pageobjects.ProductCatalogue;
+
+import org.testng.AssertJUnit;
+import java.io.IOException;
+import java.util.List;
+
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ErrorValidationsTest extends BaseTest {
+
+	@Test(groups= {"ErrorHandling"},retryAnalyzer=Retry.class)
+	public void LoginErrorValidation() throws IOException, InterruptedException {
+
+	
+		landingPage.loginApplication("anshika@gmail.com", "Iamki000");
+		Assert.assertEquals("Incorrect email or password.", landingPage.getErrorMessage());
+
+	}
+	
+
+	@Test
+	public void ProductErrorValidation() throws IOException, InterruptedException
+	{
+
+		String productName = "ZARA COAT 3";
+		ProductCatalogue productCatalogue = landingPage.loginApplication("rahulshetty@gmail.com", "Iamking@000");
+		List<WebElement> products = productCatalogue.getProductList();
+		productCatalogue.addProductToCart(productName);
+		CartPage cartPage = productCatalogue.goToCartPage();
+		Boolean match = cartPage.VerifyProductDisplay("ZARA COAT 33");
+		Assert.assertFalse(match);
+		
+	
+
+	}
+
+	
+	
+
+}
